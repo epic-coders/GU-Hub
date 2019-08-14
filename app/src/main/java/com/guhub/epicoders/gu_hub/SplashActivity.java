447fc54750f42853c82661eac1e07ac5a286ad85
ankush-01 <ankush9044045461@gmail.com>
2019-08-15 01:26:31 +0530

    Splash Screen

--- app/src/main/java/com/guhub/epicoders/gu_hub/SplashActivity.java
+++ app/src/main/java/com/guhub/epicoders/gu_hub/SplashActivity.java
@@ -1,5 +1,7 @@
 package com.guhub.epicoders.gu_hub;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,23 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        Handler handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        finishSplash();
+                    }
+                });
+            }
+        },3000);
+
+    }
+    protected void finishSplash(){
+        this.finish();
     }
 }
